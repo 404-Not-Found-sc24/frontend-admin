4f0fdfe586a35ff499ce888f3112f7340b1a49ad
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-05-11 16:05:06 +0900

    fix: package.json/react-router-dom

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 const Main: React.FC = () => {
-
-    return (
-        <div className="w-5/6 ml-[240px] h-[900px] flex-1 flex justify-center flex-col items-center text-main-green-color text-5xl">
-            나는 메인페이지다~
-        </div>
-    );
+  return (
+    <div className="w-5/6 ml-[240px] h-[900px] flex-1 flex justify-center flex-col items-center text-main-green-color text-5xl">
+      나는 메인페이지다~
+    </div>
+  );
 };
 
 export default Main;
